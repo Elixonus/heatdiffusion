688c8cd0754f9cd683a7713ecfb0bf8442e1f82b
Elias <elias@dobrin.net>
2022-04-29 21:51:06 -0700

    got donut and square working :DD

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import hypot
 import matplotlib.pyplot as plt
 from heatdiff import System, Element
 
@@ -6,10 +7,20 @@ system = System(grid=(100, 100), size=(1, 1), temperature=300, conductivity=80)
 for gx in range(system.grid[0]):
     for gy in range(system.grid[1]):
         element = system.elements[gx][gy]
-        if 40 <= gx <= 60 and 40 <= gy <= 60:
+        rx = gx / (system.grid[0] - 1)
+        ry = gy / (system.grid[1] - 1)
+        r = hypot(rx - 0.5, ry - 0.5)
+        if 0.1 < r < 0.2:
             element.temperature = 500
+for gx in range(system.grid[0]):
+    for gy in range(system.grid[1]):
+        element = system.elements[gx][gy]
+        rx = gx / (system.grid[0] - 1)
+        ry = gy / (system.grid[1] - 1)
+        if 0.2 < rx < 0.3 and 0.3 < ry < 0.4:
+            element.temperature = 700
 
-for i in range(10):
+for i in range(100):
     system.diffuse(delta_time=0.001)
 
 
