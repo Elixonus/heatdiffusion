f64d13bb30369ed91c58966561224d1ae4fbb7aa
Elias <elias@dobrin.net>
2022-04-29 21:25:15 -0700

    heat conduction transfer working

--- heatdiff.py
+++ heatdiff.py
@@ -7,31 +7,31 @@ class System:
     size: tuple[float, float]
 
     def __init__(self, grid: tuple[int, int], size: tuple[float, float], temperature: float, conductivity: float) -> None:
-        self.elements = [[Element(temperature, conductivity) for y in range(grid[0])] for x in range(grid[1])]
+        self.elements = [[Element(temperature, conductivity) for gy in range(grid[0])] for gx in range(grid[1])]
         self.grid = grid
         self.size = size
 
     def diffuse(self, delta_time: float) -> None:
-        for x in range(self.grid[0]):
-            for y in range(self.grid[1]):
-                element = self.elements[x][y]
-                if x > 0:
-                    element.diffuse(self.elements[x - 1][y],
+        for gx in range(self.grid[0]):
+            for gy in range(self.grid[1]):
+                element = self.elements[gx][gy]
+                if gx > 0:
+                    element.diffuse(self.elements[gx - 1][gy],
                                     area=self.size[1] / self.grid[1],
                                     distance=self.size[0] / self.grid[0],
                                     delta_time=delta_time)
-                if x < self.grid[0] - 1:
-                    element.diffuse(self.elements[x + 1][y],
+                if gx < self.grid[0] - 1:
+                    element.diffuse(self.elements[gx + 1][gy],
                                     area=self.size[1] / self.grid[1],
                                     distance=self.size[0] / self.grid[0],
                                     delta_time=delta_time)
-                if y > 0:
-                    element.diffuse(self.elements[x][y - 1],
+                if gy > 0:
+                    element.diffuse(self.elements[gx][gy - 1],
                                     area=self.size[0] / self.grid[0],
                                     distance=self.size[1] / self.grid[1],
                                     delta_time=delta_time)
-                if y < self.grid[1] - 1:
-                    element.diffuse(self.elements[x][y + 1],
+                if gy < self.grid[1] - 1:
+                    element.diffuse(self.elements[gx][gy + 1],
                                     area=self.size[0] / self.grid[0],
                                     distance=self.size[1] / self.grid[1],
                                     delta_time=delta_time)
@@ -46,7 +46,8 @@ class Element:
         self.conductivity = conductivity
 
     def diffuse(self, element: Element, area: float, distance: float, delta_time: float) -> None:
-        conductivity = (self.conductivity + element.conductivity) / 2
         temperature_difference = element.temperature - self.temperature
+        conductivity = (self.conductivity + element.conductivity) / 2
         rate = (conductivity * area * temperature_difference) / distance
         self.temperature += rate * delta_time
+        element.temperature -= rate * delta_time
---
+++ main.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from heatdiff import System, Element
+
+system = System(grid=(100, 100), size=(1, 1), temperature=300, conductivity=80)
+
+for gx in range(system.grid[0]):
+    for gy in range(system.grid[1]):
+        element = system.elements[gx][gy]
+        if 40 <= gx <= 60 and 40 <= gy <= 60:
+            element.temperature = 500
+
+for i in range(100):
+    for gx in range(system.grid[0]):
+        for gy in range(system.grid[1]):
+            element = system.elements[gx][gy]
+            if 40 <= gx <= 60 and 40 <= gy <= 60:
+                element.temperature = 500
+    system.diffuse(delta_time=0.01)
+
+
+
+temperatures = [[system.elements[x][y].temperature for y in range(system.grid[1])] for x in range(system.grid[0])]
+fig, ax = plt.subplots()
+ax.imshow(temperatures, origin="lower")
+plt.show()
