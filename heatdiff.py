c86aebb300c4119be25bb33350da3d243e14ebab
Elias <elias@dobrin.net>
2022-04-29 20:59:56 -0700

    did diffusion method for system

--- heatdiff.py
+++ heatdiff.py
@@ -3,18 +3,38 @@ from __future__ import annotations
 
 class System:
     elements: list[list[Element]]
-    width: int
-    height: int
+    grid: tuple[int, int]
+    size: tuple[float, float]
 
-    def __init__(self, width: int, height: int, temperature: float, conductivity: float) -> None:
-        self.elements = [[Element(temperature, conductivity) for y in range(height)] for x in range(width)]
-        self.width = width
-        self.height = height
+    def __init__(self, grid: tuple[int, int], size: tuple[float, float], temperature: float, conductivity: float) -> None:
+        self.elements = [[Element(temperature, conductivity) for y in range(grid[0])] for x in range(grid[1])]
+        self.grid = grid
+        self.size = size
 
     def diffuse(self, delta_time: float) -> None:
-        for x in range(1, self.width - 1):
-            for y in range(1, self.height - 1):
-
+        for x in range(self.grid[0]):
+            for y in range(self.grid[1]):
+                element = self.elements[x][y]
+                if x > 0:
+                    element.diffuse(self.elements[x - 1][y],
+                                    area=self.size[1] / self.grid[1],
+                                    distance=self.size[0] / self.grid[0],
+                                    delta_time=delta_time)
+                if x < self.grid[0] - 1:
+                    element.diffuse(self.elements[x + 1][y],
+                                    area=self.size[1] / self.grid[1],
+                                    distance=self.size[0] / self.grid[0],
+                                    delta_time=delta_time)
+                if y > 0:
+                    element.diffuse(self.elements[x][y - 1],
+                                    area=self.size[0] / self.grid[0],
+                                    distance=self.size[1] / self.grid[1],
+                                    delta_time=delta_time)
+                if y < self.grid[1] - 1:
+                    element.diffuse(self.elements[x][y + 1],
+                                    area=self.size[0] / self.grid[0],
+                                    distance=self.size[1] / self.grid[1],
+                                    delta_time=delta_time)
 
 
 class Element:
