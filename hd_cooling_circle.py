a6fc2438f170529f225b2db3e3ad587ed6b341d3
Daxonus <elias@dobrin.net>
2022-05-01 17:49:21 -0700

    added donut example

--- hd_cooling_circle.py
+++ hd_cooling_circle.py
@@ -5,6 +5,6 @@ print("Solving heat conduction equation...")
 system = System(grid=(100, 100), size=(1, 1), temperature=300, conductivity=80)
 system.circle(center=(0.5, 0.5), radius=0.2, temperature=700)
 system_0 = system.save()
-system.diffuses(time=0.03, steps=100)
+system.diffuses(time=0.1, steps=100)
 render(systems=[system_0, system],
        show_conductivity=False)
--- hd_cooling_donut.py
+++ hd_cooling_donut.py
@@ -3,9 +3,9 @@ from render import render
 
 print("Solving heat conduction equation...")
 system = System(grid=(100, 100), size=(1, 1), temperature=300, conductivity=80)
-system.square(center=(0.5, 0.5), size=0.4, temperature=700)
+system.donut(center=(0.5, 0.5), radius_outer=0.2, radius_inner=0.1, temperature=700)
 system_0 = system.save()
-system.diffuses(time=0.03, steps=50)
+system.diffuses(time=0.05, steps=50)
 system_1 = system.save()
 system.diffuses(time=0.1, steps=100)
 render(systems=[system_0, system_1, system],
