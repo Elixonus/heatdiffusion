2d16f38ae05191523eaa2536492dd12795ab966b
Daxonus <elias@dobrin.net>
2022-05-01 18:14:36 -0700

    added example with thermal bridge

---
+++ hd_cooling_bars_diff_bridge.py
@@ -0,0 +1,16 @@
+from heatdiffusion import System
+from render import render
+
+print("Solving heat conduction equation...")
+system = System(grid=(100, 100), size=(1, 1), temperature=300, conductivity=80)
+system.rectangle(center=(0.5, 0.3), size=(0.3, 0.1), temperature=1000)
+system.rectangle(center=(0.5, 0.7), size=(0.4, 0.1), temperature=700)
+system.rectangle(center=(0.5, 0.5), size=(0.1, 0.3), conductivity=300)
+system_0 = system.save()
+system.diffuses(time=0.03, steps=200)
+system_1 = system.save()
+system.diffuses(time=0.03, steps=200)
+system_2 = system.save()
+system.diffuses(time=0.2, steps=800)
+render(systems=[system_0, system_1, system_2, system],
+       show_conductivity=True)
--- render.py
+++ render.py
@@ -21,7 +21,7 @@ def render(*, systems: list[System], show_conductivity: bool) -> None:
         for t, conductivity in reversed(list(enumerate(conductivities))):
             system = systems[t]
             fig, ax = plt.subplots()
-            im = ax.imshow(conductivity, origin="lower", extent=(0, system.size[0], 0, system.size[1]), interpolation="bicubic")
+            im = ax.imshow(conductivity, origin="lower", extent=(0, system.size[0], 0, system.size[1]))
             ax.set_title(f"Map of conductivity coefficients at time t={system.time:.4f} sec")
             ax.set_xlabel("Horizontal displacement (meters)")
             ax.set_ylabel("Vertical displacement (meters)")
