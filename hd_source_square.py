2a0ad16c9e75b6d38367a92aef77c7581e080a05
Daxonus <elias@dobrin.net>
2022-05-01 18:24:31 -0700

    added other source examples: bars donut square

---
+++ hd_source_square.py
@@ -0,0 +1,17 @@
+from heatdiffusion import System
+from render import render
+
+
+def apply_boundary(time: float) -> None:
+    system.square(center=(0.5, 0.5), size=0.4, temperature=700)
+
+
+print("Solving heat conduction equation...")
+system = System(grid=(100, 100), size=(1, 1), temperature=300, conductivity=80)
+apply_boundary(0)
+system_0 = system.save()
+system.diffuses(time=0.06, steps=50, do_every_step=apply_boundary)
+system_1 = system.save()
+system.diffuses(time=0.2, steps=200, do_every_step=apply_boundary)
+render(systems=[system_0, system_1, system],
+       show_conductivity=False)
