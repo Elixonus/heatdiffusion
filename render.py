e6b73e421a457b5673b5b898bd1ecb57cb7709d9
Daxonus <elias@dobrin.net>
2022-05-10 09:06:41 -0700

    made temperature every display show with the same limits

--- render.py
+++ render.py
@@ -13,8 +13,11 @@ def render(*, systems: list[System], show_conductivity: bool) -> None:
                         for x in range(system.grid[0])]
                        for y in range(system.grid[1])]
         temperatures.append(temperature)
+    temperatures_sort = sorted(temperature for temperatures_buffer in temperatures[0] for temperature in temperatures_buffer)
+    temperature_low = temperatures_sort[round(0.01 * (len(temperatures_sort) - 1))]
+    temperature_high = temperatures_sort[round(0.99 * (len(temperatures_sort) - 1))]
 
-    if show_conductivity and len(systems) > 0:
+    if show_conductivity:
         system = systems[0]
         conductivity = [[system.elements[x][y].conductivity
                          for x in range(system.grid[0])]
@@ -30,7 +33,7 @@ def render(*, systems: list[System], show_conductivity: bool) -> None:
     for t, temperature in reversed(list(enumerate(temperatures))):
         system = systems[t]
         fig, ax = plt.subplots()
-        im = ax.imshow(temperature, origin="lower", extent=(0, system.size[0], 0, system.size[1]), cmap="inferno", interpolation="bicubic")
+        im = ax.imshow(temperature, origin="lower", extent=(0, system.size[0], 0, system.size[1]), vmin=temperature_low, vmax=temperature_high, cmap="inferno", interpolation="bicubic")
         ax.set_title(f"Map of temperatures at time t={system.time:.4f} sec")
         ax.set_xlabel("Horizontal displacement (meters)")
         ax.set_ylabel("Vertical displacement (meters)")
